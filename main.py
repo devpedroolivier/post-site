165188de8566bdda5ceb6af60c35521da18f8606
Pedro Oliveira <pedrogabrielcosta7@gmail.com>
2025-06-17 14:54:37 -0300

    Atualização do main.py

--- main.py
+++ main.py
@@ -62,3 +62,7 @@ async def servicos(request: Request):
 async def quem_somos(request: Request):
     return templates.TemplateResponse("quem_somos.html", {"request": request})
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
